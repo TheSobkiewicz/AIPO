0e0c09fcbc217bad79b86a64ce464080b6125b5e
TheSobkiewicz <sobkiewiczt@gmail.com>
2024-06-22 15:30:41 +0200

    Changed to deepcopy

--- demo.py
+++ demo.py
@@ -2,13 +2,14 @@ from photo_checker import get_person_document
 from data_extractor import get_data_from_image
 import uuid
 import cv2
+import copy
 image = cv2.imread("data/documents/document_1.png")
 
 # Function to process the document, check if it is already in the database, and save it if not. Returns all documents in the database with the same face and the extracted fields from given image.
 def process_document(image):
     all_documents= get_person_document(image)
     document_fields = get_data_from_image(image)
-    all_documents_copy = all_documents.copy()
+    all_documents_copy = copy.deepcopy(all_documents)
     for document in all_documents_copy:
         document.pop('FILENAME', None)
     if document_fields != {} and document_fields not in all_documents_copy:
