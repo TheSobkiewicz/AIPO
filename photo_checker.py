8d4026ec90a8ec11846b8a5fa882c6f19b329724
Viktoriia Kinash <viktoriiakinash@gmail.com>
2024-06-23 20:31:13 +0200

    add hyperparameter

--- photo_checker.py
+++ photo_checker.py
@@ -3,7 +3,7 @@ from deepface import DeepFace
 
 #Function that check if the person is already in the database and returns all documents with the same face.
 def get_person_document(image):
-    imgs = DeepFace.find(img_path = image, db_path = "./data/documents", enforce_detection = False)
+    imgs = DeepFace.find(img_path = image, db_path = "./data/documents", enforce_detection = False, detector_backend="mtcnn")
     if imgs[0].empty:
         return []
     ids = set(imgs[0]['identity'].values.tolist())
